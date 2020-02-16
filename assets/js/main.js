14ed0e2e328cfb2955d4a8519394eab0ed5d03b0
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2020-02-16 17:44:20 +0800

    Fix API url base

--- assets/js/main.js
+++ assets/js/main.js
@@ -131,7 +131,7 @@ function search() {
   var searchResult = $('.search-result');
   var popular = $('.popular-wrapper');
 
-  var base = window.location.protocol + "//" + window.location.host;
+  var base = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname.split('/')[1];
   var url = base + '/ghost/api/v2/content/posts/?key=' + search_key + '&limit=all&fields=id,title,url,updated_at,visibility&order=updated_at%20desc&formats=plaintext';
   var indexDump = JSON.parse(localStorage.getItem('dawn_index'));
   var index;
