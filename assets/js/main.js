7f2c5f57beac5bf1563e4268782ac1290e14861d
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2020-03-20 22:01:52 +0800

    Add lightbox

--- assets/js/main.js
+++ assets/js/main.js
@@ -126,6 +126,8 @@ function gallery() {
     var ratio = width / height;
     container.style.flex = ratio + ' 1 0%';
   });
+
+  pswp('.kg-gallery-container', '.kg-gallery-image', '.kg-gallery-image', false, true);
 }
 
 function table() {
@@ -365,6 +367,81 @@ function theme() {
     } else {
       dark();
     }
+  });
+}
+
+function pswp(container, element, trigger, caption, isGallery) {
+  var parseThumbnailElements = function (el) {
+    var items = [],
+      gridEl,
+      linkEl,
+      item;
+
+    $(el).find(element).each(function (i, v) {
+      gridEl = $(v);
+      linkEl = gridEl.find(trigger);
+
+      item = {
+        src: isGallery ? gridEl.find('img').attr('src') : linkEl.attr('href'),
+        w: 0,
+        h: 0,
+      };
+
+      if (caption && gridEl.find(caption).length) {
+        item.title = gridEl.find(caption).html();
+      }
+
+      items.push(item);
+    });
+
+    return items;
+  };
+
+  var openPhotoSwipe = function(index, galleryElement) {
+    var pswpElement = document.querySelectorAll('.pswp')[0],
+        gallery,
+        options,
+        items;
+
+    items = parseThumbnailElements(galleryElement);
+
+    options = {
+      closeOnScroll: false,
+      history: false,
+      index: index,
+      shareEl: false,
+      showAnimationDuration: 0,
+			showHideOpacity: true,
+    };
+
+    gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.listen('gettingData', function (index, item) {
+      if (item.w < 1 || item.h < 1) { // unknown size
+        var img = new Image(); 
+        img.onload = function() { // will get size after load
+          item.w = this.width; // set image width
+          item.h = this.height; // set image height
+          gallery.updateSize(true); // reinit Items
+        }
+        img.src = item.src; // let's download image
+      }
+    });
+    gallery.init();
+  };
+
+  var onThumbnailsClick = function(e) {
+    e.preventDefault();
+
+    var index = $(e.target).closest(container).find(element).index($(e.target).closest(element))
+    var clickedGallery = $(e.target).closest(container);
+
+    openPhotoSwipe(index, clickedGallery[0]);
+
+    return false;
+  };
+
+  $(container).on('click', trigger, function (e) {
+    onThumbnailsClick(e);
   });
 }
 
