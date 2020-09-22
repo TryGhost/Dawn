e781457af5477bf0cee23f7e51515669a0c46ac7
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2020-09-22 14:04:37 +0800

    Cleanup gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,6 @@ const postcss = require('gulp-postcss');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const beeper = require('beeper');
-const fs = require('fs');
 
 // postcss plugins
 const easyimport = require('postcss-easy-import');
