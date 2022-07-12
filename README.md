505e21f9a2ea460a67d77a5c5d0b5771b498db0a
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2022-07-12 20:09:02 +0800

    Migration (#98)
    * Add Alto theme
    
    * Remove unnecessary specificity from link hover selector
    
    * Add Dope theme
    
    * Refine Dope theme
    
    * Remove unnecessary selectors from dark mode styles
    
    * Add Ruby theme
    
    * Add branding+navigation header style
    
    * Post/page template cleanup for Ruby theme
    
    * Remove jQuery dependency from Ruby
    
    * Photoswipe for Ruby
    
    * package.json cleanup
    
    * Add Ease theme
    
    * Ease theme cleanup
    
    * Add Edge theme
    
    * Add callback to pagination script
    
    * Rebuild assets
    
    * Refresh Wave
    
    * Wave theme pagination
    
    * Remove unused settings
    
    * Add tag/author pages to Wave
    
    * Remove unnecessary dependencies
    
    * Post meta spacing
    
    * Dawn theme
    
    * Edition theme
    
    * Cleanup body class
    
    * Lint
    
    * More cleanup
    
    * London theme
    
    * Ease theme search cleanup
    
    * Minor bug fixes
    
    * Remove jQuery dependency from the custom search script
    
    * Put back commented functions
    
    * Update posts per page to make it consistent with homepage section posts
    
    * Alto theme update
    
    * Rebuild assets
    
    * Remove unused theme setting
    
    * Unnecessary rule
    
    * Start adding search support
    
    * Search support to more themes
    
    * Remove starter
    
    * Add Alto theme workflow

---
+++ README.md
@@ -0,0 +1,40 @@
+# Dawn
+
+A highly functional theme that adapts to the reader's preferences. Let them read, search, subscribe, navigate, and more with ease. Completely free and fully responsive, released under the MIT license.
+
+**Demo: https://dawn.ghost.io**
+
+&nbsp;
+
+# Instructions
+
+1. [Download this theme](https://github.com/TryGhost/Dawn/archive/main.zip)
+2. Log into Ghost, and go to the `Design` settings area to upload the zip file
+
+# Development
+
+Styles are compiled using Gulp/PostCSS to polyfill future CSS spec. You'll need [Node](https://nodejs.org/), [Yarn](https://yarnpkg.com/) and [Gulp](https://gulpjs.com) installed globally. After that, from the theme's root directory:
+
+```bash
+# Install
+yarn
+
+# Run build & watch for changes
+$ yarn dev
+```
+
+Now you can edit `/assets/css/` files, which will be compiled to `/assets/built/` automatically.
+
+The `zip` Gulp task packages the theme files into `dist/<theme-name>.zip`, which you can then upload to your site.
+
+```bash
+yarn zip
+```
+
+# PostCSS Features Used
+
+- Autoprefixer - Don't worry about writing browser prefixes of any kind, it's all done automatically with support for the latest 2 major versions of every browser.
+
+# Copyright & License
+
+Copyright (c) 2013-2022 Ghost Foundation - Released under the [MIT license](LICENSE).
---
+++ assets/js/main.js
@@ -0,0 +1,83 @@
+$(function () {
+    'use strict';
+    subMenu();
+    featured();
+    pagination(false);
+});
+
+function subMenu() {
+    'use strict';
+    var nav = document.querySelector('.gh-head-menu .nav');
+    var items = nav.querySelectorAll('li');
+
+    function getSiblings(el, filter) {
+        var siblings = [];
+        while (el= el.nextSibling) { if (!filter || filter(el)) siblings.push(el); }
+        return siblings;
+    }
+
+    function exampleFilter(el) {
+        return el.nodeName.toLowerCase() == 'li';
+    }
+
+    if (items.length > 5) {
+        var separator = items[4];
+
+        var toggle = document.createElement('button');
+        toggle.setAttribute('class', 'button-icon menu-item-button menu-item-more');
+        toggle.setAttribute('aria-label', 'More');
+        toggle.innerHTML = '<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M21.333 16c0-1.473 1.194-2.667 2.667-2.667v0c1.473 0 2.667 1.194 2.667 2.667v0c0 1.473-1.194 2.667-2.667 2.667v0c-1.473 0-2.667-1.194-2.667-2.667v0zM13.333 16c0-1.473 1.194-2.667 2.667-2.667v0c1.473 0 2.667 1.194 2.667 2.667v0c0 1.473-1.194 2.667-2.667 2.667v0c-1.473 0-2.667-1.194-2.667-2.667v0zM5.333 16c0-1.473 1.194-2.667 2.667-2.667v0c1.473 0 2.667 1.194 2.667 2.667v0c0 1.473-1.194 2.667-2.667 2.667v0c-1.473 0-2.667-1.194-2.667-2.667v0z"></path></svg>';
+
+        var wrapper = document.createElement('div');
+        wrapper.setAttribute('class', 'sub-menu');
+
+        var children = getSiblings(separator, exampleFilter);
+
+        children.forEach(function (child) {
+            wrapper.appendChild(child);
+        });
+
+        toggle.appendChild(wrapper);
+        separator.parentNode.appendChild(toggle);
+
+        toggle.addEventListener('click', function () {
+            if (window.getComputedStyle(wrapper).display == 'none') {
+                wrapper.style.display = 'block';
+                wrapper.classList.add('animate__animated', 'animate__bounceIn');
+            } else {
+                wrapper.classList.add('animate__animated', 'animate__zoomOut');
+            }
+        });
+
+        wrapper.addEventListener('animationend', function (e) {
+            wrapper.classList.remove('animate__animated', 'animate__bounceIn', 'animate__zoomOut');
+            if (e.animationName == 'zoomOut') {
+                wrapper.style.display = 'none';
+            }
+        });
+    }
+}
+
+function featured() {
+    'use strict';
+    $('.featured-feed').owlCarousel({
+        dots: false,
+        margin: 30,
+        nav: true,
+        navText: [
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" class="icon"><path d="M20.547 22.107L14.44 16l6.107-6.12L18.667 8l-8 8 8 8 1.88-1.893z"></path></svg>',
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" class="icon"><path d="M11.453 22.107L17.56 16l-6.107-6.12L13.333 8l8 8-8 8-1.88-1.893z"></path></svg>',
+        ],
+        responsive: {
+            0: {
+                items: 1,
+            },
+            768: {
+                items: 2,
+            },
+            992: {
+                items: 3,
+            },
+        },
+    });
+}
