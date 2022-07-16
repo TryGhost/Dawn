53c0dedaa4038e62f13fa63b2abe1288366f1664
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2022-07-17 00:15:25 +0800

    Add development workflow to all themes

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+const {series, parallel, watch, src, dest} = require('gulp');
+const pump = require('pump');
+
+// gulp plugins and utils
+const livereload = require('gulp-livereload');
+const postcss = require('gulp-postcss');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const beeper = require('beeper');
+const zip = require('gulp-zip');
+
+// postcss plugins
+const easyimport = require('postcss-easy-import');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+function serve(done) {
+    livereload.listen();
+    done();
+}
+
+function handleError(done) {
+    return function (err) {
+        if (err) {
+            beeper();
+        }
+        return done(err);
+    };
+};
+
+function hbs(done) {
+    pump([
+        src(['*.hbs', 'partials/**/*.hbs']),
+        livereload()
+    ], handleError(done));
+}
+
+function css(done) {
+    pump([
+        src('assets/css/screen.css', {sourcemaps: true}),
+        postcss([
+            easyimport,
+            autoprefixer(),
+            cssnano()
+        ]),
+        dest('assets/built/', {sourcemaps: '.'}),
+        livereload()
+    ], handleError(done));
+}
+
+function js(done) {
+    pump([
+        src([
+            'assets/js/lib/*.js',
+            'assets/js/main.js'
+        ], {sourcemaps: true}),
+        concat('main.min.js'),
+        uglify(),
+        dest('assets/built/', {sourcemaps: '.'}),
+        livereload()
+    ], handleError(done));
+}
+
+function zipper(done) {
+    const filename = require('./package.json').name + '.zip';
+
+    pump([
+        src([
+            '**',
+            '!node_modules', '!node_modules/**',
+            '!dist', '!dist/**',
+            '!yarn-error.log'
+        ]),
+        zip(filename),
+        dest('dist/')
+    ], handleError(done));
+}
+
+const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs'], hbs);
+const cssWatcher = () => watch('assets/css/**/*.css', css);
+const jsWatcher = () => watch('assets/js/**/*.js', js);
+const watcher = parallel(hbsWatcher, cssWatcher, jsWatcher);
+const build = series(css, js);
+
+exports.build = build;
+exports.zip = series(build, zipper);
+exports.default = series(build, serve, watcher);
