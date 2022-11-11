8cef49f7753c023075cb7a63fea46e92c08ad0e5
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2022-11-11 12:02:51 +0800

    Added navbar options (#187)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,65 +1,8 @@
 $(function () {
     'use strict';
-    subMenu();
     featured();
     pagination(false);
 });
-
-function subMenu() {
-    'use strict';
-
-    var nav = document.querySelector('.gh-head-menu .nav');
-    if (!nav) return;
-
-    var items = nav.querySelectorAll('li');
-
-    function getSiblings(el, filter) {
-        var siblings = [];
-        while (el= el.nextSibling) { if (!filter || filter(el)) siblings.push(el); }
-        return siblings;
-    }
-
-    function exampleFilter(el) {
-        return el.nodeName.toLowerCase() == 'li';
-    }
-
-    if (items.length > 5) {
-        var separator = items[4];
-
-        var toggle = document.createElement('button');
-        toggle.setAttribute('class', 'button-icon menu-item-button menu-item-more');
-        toggle.setAttribute('aria-label', 'More');
-        toggle.innerHTML = '<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M21.333 16c0-1.473 1.194-2.667 2.667-2.667v0c1.473 0 2.667 1.194 2.667 2.667v0c0 1.473-1.194 2.667-2.667 2.667v0c-1.473 0-2.667-1.194-2.667-2.667v0zM13.333 16c0-1.473 1.194-2.667 2.667-2.667v0c1.473 0 2.667 1.194 2.667 2.667v0c0 1.473-1.194 2.667-2.667 2.667v0c-1.473 0-2.667-1.194-2.667-2.667v0zM5.333 16c0-1.473 1.194-2.667 2.667-2.667v0c1.473 0 2.667 1.194 2.667 2.667v0c0 1.473-1.194 2.667-2.667 2.667v0c-1.473 0-2.667-1.194-2.667-2.667v0z"></path></svg>';
-
-        var wrapper = document.createElement('div');
-        wrapper.setAttribute('class', 'sub-menu');
-
-        var children = getSiblings(separator, exampleFilter);
-
-        children.forEach(function (child) {
-            wrapper.appendChild(child);
-        });
-
-        toggle.appendChild(wrapper);
-        separator.parentNode.appendChild(toggle);
-
-        toggle.addEventListener('click', function () {
-            if (window.getComputedStyle(wrapper).display == 'none') {
-                wrapper.style.display = 'block';
-                wrapper.classList.add('animate__animated', 'animate__bounceIn');
-            } else {
-                wrapper.classList.add('animate__animated', 'animate__zoomOut');
-            }
-        });
-
-        wrapper.addEventListener('animationend', function (e) {
-            wrapper.classList.remove('animate__animated', 'animate__bounceIn', 'animate__zoomOut');
-            if (e.animationName == 'zoomOut') {
-                wrapper.style.display = 'none';
-            }
-        });
-    }
-}
 
 function featured() {
     'use strict';
