5c6f21742fe5015d98fd9fbcd2d3b6837fbf6503
Adin <akvashi24@gmail.com>
2024-09-14 12:26:57 -0400

    Populate settings

---
+++ assets/js/settings.js
@@ -0,0 +1,7 @@
+const cards = [
+    {
+        title: 'Distros',
+        subtitle: 'We serve tea here sometimes',
+        image: 'https://www.gpb.org/sites/default/files/2024-06/062124_food_distro_01.jpg'
+    }
+]
