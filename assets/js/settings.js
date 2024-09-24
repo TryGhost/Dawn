fa5dd7a8895535d5115effba5862bb4e46546471
Adin <akvashi24@gmail.com>
2024-09-23 21:06:45 -0400

    We don't use subtitle anymore bozo

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -10,13 +10,11 @@
 // const cards = [
 //     {
 //         title: 'Distros',
-//         subtitle: 'We serve tea here sometimes',
 //         image: 'https://www.gpb.org/sites/default/files/2024-06/062124_food_distro_01.jpg',
 //         url: 'http://theteastand.org/distros/' 
 //     },
 //     {
 //         title: 'Popups',
-//         subtitle: 'We serve tea here too',
 //         image: 'https://www.gpb.org/sites/default/files/2024-06/062124_food_distro_01.jpg',
 //         url: 'https://theteastand.org/popups/' 
 //     },
