505053a0a1a1d9aabbb67b42d987479d37afe16d
Adin Vashi <akvashi24@gmail.com>
2024-09-23 20:44:38 -0400

    Revert settings.js

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -11,10 +11,5 @@
 // ]
 
 const cards = [
-    {
-        title: 'Distros',
-        subtitle: 'We serve tea here sometimes',
-        image: 'https://www.gpb.org/sites/default/files/2024-06/062124_food_distro_01.jpg',
-        url: 'http://localhost:2368/about/'
-    },
+
 ]
