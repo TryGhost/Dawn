8365c6ea602b9f2f9e4b0b8b85186cd8a5244e28
Adin <akvashi24@gmail.com>
2024-09-23 20:29:24 -0400

    Update styling and links

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,9 +1,15 @@
-console.log('Loading custom settings...')
+
+// Modality cards on homepage
+// e.g.
+// const cards = [
+//     {
+//         title: 'Distros',
+//         subtitle: 'We serve tea here sometimes',
+//         image: 'https://www.gpb.org/sites/default/files/2024-06/062124_food_distro_01.jpg',
+//         url: 'http://localhost:2368/about/'
+//     },
+// ]
+
 const cards = [
-    {
-        title: 'Distros',
-        subtitle: 'We serve tea here sometimes',
-        image: 'https://www.gpb.org/sites/default/files/2024-06/062124_food_distro_01.jpg'
-    }
+    
 ]
-console.log('Custom settings loaded!')
