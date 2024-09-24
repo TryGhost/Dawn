4752b16e36522bb211848cad0dc9243750984fb3
Adin <akvashi24@gmail.com>
2024-09-23 20:57:38 -0400

    Test cards

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,15 +1,12 @@
 
 // Modality cards on homepage
 // e.g.
-// const cards = [
-//     {
-//         title: 'Distros',
-//         subtitle: 'We serve tea here sometimes',
-//         image: 'https://www.gpb.org/sites/default/files/2024-06/062124_food_distro_01.jpg',
-//         url: 'http://localhost:2368/about/'
-//     },
-// ]
-
 const cards = [
-
+    {
+        title: 'Distros',
+        subtitle: 'We serve tea here sometimes',
+        image: 'https://www.gpb.org/sites/default/files/2024-06/062124_food_distro_01.jpg',
+        url: 'http://localhost:2368/about/'
+    },
 ]
+
