f60b0b7982f725510066dd3e8b5d9773b3a81e3f
Adin <akvashi24@gmail.com>
2024-09-23 21:00:56 -0400

    Revert cards

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,12 +1,21 @@
 
 // Modality cards on homepage
+//
+// Notes:
+// Make sure cards.url is a full link!
+// Notice those quotations
+// Hit me if you have any questions
+// 
 // e.g.
-const cards = [
-    {
-        title: 'Distros',
-        subtitle: 'We serve tea here sometimes',
-        image: 'https://www.gpb.org/sites/default/files/2024-06/062124_food_distro_01.jpg',
-        url: 'http://localhost:2368/about/'
-    },
-]
+// const cards = [
+//     {
+//         title: 'Distros',
+//         subtitle: 'We serve tea here sometimes',
+//         image: 'https://www.gpb.org/sites/default/files/2024-06/062124_food_distro_01.jpg',
+//         url: 'http://localhost:2368/about/' 
+//     },
+// ]
 
+const cards = [
+    
+]
