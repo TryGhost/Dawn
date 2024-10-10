bc0bc62a4f23fcfbf2ba85bd5a180010cec52cb9
Adin <akvashi24@gmail.com>
2024-10-10 13:14:51 -0400

    Use Ghost's assets syntax

---
+++ assets/js/settings.js
@@ -0,0 +1,43 @@
+// Modality cards on homepage
+//
+// Notes:
+// Make sure cards.url is a full link!
+// Notice those quotations
+// Hit me if you have any questions
+// 
+// e.g.
+// const cards = [
+//     {
+//         title: 'Distros',
+//         image: 'https://www.gpb.org/sites/default/files/2024-06/062124_food_distro_01.jpg',
+//         url: 'http://theteastand.org/distros/' 
+//     },
+//     {
+//         title: 'Popups',
+//         image: 'https://www.gpb.org/sites/default/files/2024-06/062124_food_distro_01.jpg',
+//         url: 'https://theteastand.org/popups/' 
+//     },
+// ]
+
+const cards = [
+    {
+        title: "⛩️ Popups",
+        url: "https://www.theteastand.org/popups/",
+        image: "https://i.imgur.com/cjggPYy.jpeg"
+    },
+    {
+        title: "🥬 Distros",
+        url: "https://www.theteastand.org/distros/",
+        image: "https://i.imgur.com/ozD6q0p.jpeg"
+    },
+    {
+        title: "🫖 Tea Talks",
+        url: "https://www.theteastand.org/tea-talks/",
+        image: "https://i.imgur.com/oV1PqD6.jpeg"
+    },
+    {
+        title: "📀 Steeped in Sound",
+        url: "https://www.theteastand.org/steeped-in-sound/",
+        image: "https://i.imgur.com/CywIxsR.jpeg"
+    },
+]
