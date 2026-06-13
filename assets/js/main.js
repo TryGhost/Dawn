e85fe56f60c0ed82f14af1b8302262960c4cf060
canin carlos <canin@dreamfreely.org>
2026-06-13 10:04:33 -0500

    updated to current deploy

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+$(function () {
+    'use strict';
+    featured();
+    pagination(false);
+});
+
+function featured() {
+    'use strict';
+    $('.featured-feed').owlCarousel({
+        dots: false,
+        margin: 30,
+        nav: true,
+        navText: [
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" class="icon"><path d="M20.547 22.107L14.44 16l6.107-6.12L18.667 8l-8 8 8 8 1.88-1.893z"></path></svg>',
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" class="icon"><path d="M11.453 22.107L17.56 16l-6.107-6.12L13.333 8l8 8-8 8-1.88-1.893z"></path></svg>',
+        ],
+        responsive: {
+            0: {
+                items: 1,
+            },
+            768: {
+                items: 2,
+            },
+            992: {
+                items: 3,
+            },
+        },
+    });
+}
